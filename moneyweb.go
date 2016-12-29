9a2db32bda0e6dea9a7174684977e1716c53b38e
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2016-12-29 21:37:56 -0200

    I think I'm using cookies wrong

--- moneyweb.go
+++ moneyweb.go
@@ -3,14 +3,28 @@ package main
 import "os"
 import "net/http"
 import "github.com/ishiikurisu/moneyweb/view"
-// import "fmt"
+import "fmt"
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    cookies := r.Cookies()
+    cookies := r.Header.Get("username")
     username := r.FormValue("username")
 
     if len(cookies) == 0 && len(username) == 0 {
         view.SayHello(w)
+    } else if len(cookies) == 0 && len(username) > 0 {
+        password := r.FormValue("password")
+        rawCookie := fmt.Sprintf("%s=%s", username, password)
+        cookie := http.Cookie{
+            Name: "username",
+            Value: username,
+            MaxAge: 86400,
+            Secure: true,
+            HttpOnly: false,
+            Raw: rawCookie,
+            Unparsed: []string{rawCookie},
+        }
+        http.SetCookie(w, &cookie)
+        view.SayWelcome(w)
     } else {
         view.SayWelcome(w)
     }
@@ -21,6 +35,10 @@ func signUp(w http.ResponseWriter, r *http.Request) {
     view.SignUp(w)
 }
 
+func submit(w http.ResponseWriter, r *http.Request) {
+    
+}
+
 func main() {
     port := os.Getenv("PORT")
     if len(port) == 0 {
@@ -28,5 +46,6 @@ func main() {
     }
     http.HandleFunc("/", hello)
     http.HandleFunc("/sign_up", signUp)
+    http.HandleFunc("/submit", submit)
     http.ListenAndServe(":" + port, nil)
 }
