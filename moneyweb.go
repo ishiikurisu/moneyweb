7c91b93477ea2402de24d3aa5adeb72f18202369
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-12-29 17:01:50 -0200

    Created sign up screen

--- moneyweb.go
+++ moneyweb.go
@@ -8,11 +8,16 @@ func hello(w http.ResponseWriter, r *http.Request) {
     view.SayHello(w)
 }
 
+func signUp(w http.ResponseWriter, r *http.Request) {
+    view.SignUp(w)
+}
+
 func main() {
     port := os.Getenv("PORT")
     if len(port) == 0 {
         port = "8000"
     }
     http.HandleFunc("/", hello)
+    http.HandleFunc("/sign_up", signUp)
     http.ListenAndServe(":" + port, nil)
 }
--- view/view.go
+++ view/view.go
@@ -19,6 +19,15 @@ func GetPwd() string {
     return codePath
 }
 
+func LoadFileWithoutArgs(writer io.Writer, path string) {
+    htmlPath := GetPwd() + path
+    templ, err := template.ParseFiles(htmlPath)
+    err = templ.Execute(writer, nil)
+    if err != nil {
+        fmt.Printf("%#v\n", err)
+    }
+}
+
 /* VIEWS */
 
 // Displays the home screen
@@ -29,4 +38,9 @@ func SayHello(writer io.Writer) {
     if err != nil {
         fmt.Printf("%#v\n", err)
     }
+}
+
+// Loads sign up screen
+func SignUp(writer io.Writer) {
+    LoadFileWithoutArgs(writer, "view/sign_up.gohtml")
 }
