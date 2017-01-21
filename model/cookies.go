e548d4f7ba0759757607c2fe896edfa42e6b98f6
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-01-21 13:36:30 -0200

    Simplified storage struct

--- model/cookies.go
+++ model/cookies.go
@@ -5,7 +5,6 @@ import "net/url"
 import "net/http"
 import "net/http/cookiejar"
 import "github.com/ishiikurisu/moneylog"
-import "os"
 import "mime/multipart"
 import "bufio"
 
@@ -54,40 +53,11 @@ func NewLocalStorage() (*LocalStorage, error) {
     return storage, oops
 }
 
-// Adds a cookie to the cookiejar and saves it on the user session.
-// Cookies added here will last forever because of the serverless nature
-// of this app.
-func (storage *LocalStorage) AddCookie(w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request) {
-    username, _ := getUserAndPassword(r)
-    cookie := http.Cookie {
-        Name: "UserName",
-        Value: username,
-    }
-    storage.CookieJar.SetCookies(storage.Url, append(storage.CookieJar.Cookies(storage.Url), &cookie))
-    http.SetCookie(w, &cookie)
-    return w, r
-}
-
 // Extracts the current money log based on the store cookies. If there is no
 // log, a string of length 0 is returned.
 func (storage *LocalStorage) GetLog(w http.ResponseWriter, r *http.Request) string {
     outlet := ""
     cookie, err := r.Cookie("MoneyLog")
-
-    if err == nil {
-        outlet = cookie.Value
-    } else {
-        fmt.Println(err)
-    }
-
-    return outlet
-}
-
-// Extracts the current user based on the store cookies. If there is no user,
-// a string of length 0 is returned.
-func (storage *LocalStorage) GetUser(w http.ResponseWriter, r *http.Request) string {
-    outlet := ""
-    cookie, err := r.Cookie("UserName")
 
     if err == nil {
         outlet = cookie.Value
@@ -107,43 +77,6 @@ func stuffFromRaw(rawDescription, rawValue string) (string, float64) {
     fmt.Sscanf(rawValue, "%F", &value)
 
     return description, value
-}
-
-// Adds the given raw entry to the log.
-func (storage *LocalStorage) AddEntryFromRaw(rawDescription, rawValue string) {
-    description, value := stuffFromRaw(rawDescription, rawValue)
-    storage.MoneyLog.Add(description, value)
-}
-
-// Saves the current money log on a cookie.
-func (storage *LocalStorage) SaveLog(w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request) {
-    rawLog := storage.MoneyLog.ToString()
-    cookie := http.Cookie {
-        Name: "MoneyLog",
-        Value: rawLog,
-        MaxAge: 0,
-    }
-    http.SetCookie(w, &cookie)
-    return w, r
-}
-
-
-func (storage *LocalStorage) StoreLog(w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request) {
-    rawLog := storage.MoneyLog.ToString()
-    fp, err := os.Create(storage.LogFile)
-
-    if err != nil {
-        panic(err)
-    }
-    defer fp.Close()
-
-    _, err = fp.WriteString(rawLog)
-    if err != nil {
-        panic(err)
-    }
-    fp.Sync()
-
-    return w, r
 }
 
 func (storage *LocalStorage) AddLogFromFile(mmf multipart.File) string {
