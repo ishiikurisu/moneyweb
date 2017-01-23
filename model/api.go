9398c1b68332262488d7ee2dd89fa69e7a8b3b7b
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-01-23 14:34:36 -0200

    Adding basic remaining tools for API

--- model/api.go
+++ model/api.go
@@ -24,4 +24,24 @@ func RegisterApi() {
         balance := log.CalculateBalance()
         io.WriteString(w, stuff2raw(balance))
     })
+    http.HandleFunc("/api/get/descriptions", func (w http.ResponseWriter, r *http.Request) {
+        rawLog := r.FormValue("log")
+        log := moneylog.LogFromString(rawLog)
+        descriptions := log.GetDescriptions()
+        outlet := ""
+        for _, description := range descriptions {
+            outlet += description + ","
+        }
+        io.WriteString(w, outlet)
+    })
+    http.HandleFunc("/api/get/values", func (w http.ResponseWriter, r *http.Request) {
+        rawLog := r.FormValue("log")
+        log := moneylog.LogFromString(rawLog)
+        values := log.GetValues()
+        outlet := ""
+        for _, value := range values {
+            outlet += stuff2raw(value) + ","
+        }
+        io.WriteString(w, outlet)
+    })
 }
